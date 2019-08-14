0edddb1cf072e30c32d0c3c6ecc36cc005671798
facemanguy <john32meyer@gmail.com>
2019-08-14 14:46:00 -0700

    Updates

---
+++ precache-manifest.c5ecb2a79447f628cb0cb498cddcd535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb611c54c17933a4ca2e1b08be4adb7",
+    "url": "/poker-practice/index.html"
+  },
+  {
+    "revision": "03d210d0f02e0ee7ec72",
+    "url": "/poker-practice/static/css/main.da8bddcb.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/poker-practice/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "03d210d0f02e0ee7ec72",
+    "url": "/poker-practice/static/js/main.3508db37.chunk.js"
+  },
+  {
+    "revision": "0fdaebee0f067ce62e5b",
+    "url": "/poker-practice/static/js/runtime~main.86569ac0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/poker-practice/static/media/logo.ee7cd8ed.svg"
+  }
+]);
