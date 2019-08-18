7595b9275e39981239e6009a41647d0163e24899
John Meyer <john32meyer@gmail.com>
2019-08-18 01:35:22 -0700

    testing gh-pages build

---
+++ build/precache-manifest.277699017111c7a255f92286c80d0d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ae5d65cf782a96dff4c90e110107df",
+    "url": "/poker-practice/index.html"
+  },
+  {
+    "revision": "5f05d133d06bee148902",
+    "url": "/poker-practice/static/css/main.da8bddcb.chunk.css"
+  },
+  {
+    "revision": "008861dc4fd1663a32a0",
+    "url": "/poker-practice/static/js/2.51ae4c65.chunk.js"
+  },
+  {
+    "revision": "5f05d133d06bee148902",
+    "url": "/poker-practice/static/js/main.8ad11fef.chunk.js"
+  },
+  {
+    "revision": "0fdaebee0f067ce62e5b",
+    "url": "/poker-practice/static/js/runtime~main.86569ac0.js"
+  }
+]);
