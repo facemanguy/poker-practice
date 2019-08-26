18ffc812c6486430e839895aac9c73181a7a2c4f
facemanguy <john32meyer@gmail.com>
2019-08-25 17:15:53 -0700

    added draw card button, state change not work

--- src/Components/Poker.js
+++ src/Components/Poker.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Axios from 'axios';
 
+let currentDeck = 'new';
+
 export default class Poker extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             playerCards: [],
+            communityCards: [],
         }
     }
 
-    componentDidMount() {
-        Axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=2`, {})
+    async drawCard(){
+        Axios.get(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=1`, {})
+        .then(Response => {
+            console.log(Response.data);
+            this.setState({ communityCards: Response.data.cards});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    async componentDidMount() {
+        Axios.get(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=2`, {})
         .then(Response => {
             console.log(Response.data);
             this.setState({ playerCards: Response.data.cards});
+            currentDeck = (Response.data.deck_id)
+            console.log(currentDeck)
         })
         .catch(error => {
             console.log(error);
@@ -21,15 +37,21 @@ export default class Poker extends React.Component {
     }
 
     render() {
-         const { playerCards } = this.state;
+         const { playerCards} = this.state;
+         const { communityCards} = this.state;
          let items =[];
+         let flop =[];
+         for (const [index, amount] of communityCards.entries()) {
+            flop.push(<div><p key={index}>{amount.code}</p></div>)
+         }
          for (const [index, value] of playerCards.entries()) {
             items.push(<div><p key={index}>{value.code}</p><h1>{value.value} {value.suit}</h1><img src={value.image} alt={value.code}></img></div>)
         }
          return(
              <div>
-                 {items}
-                
+                 <div>{items}</div>
+                 <button onClick={this.drawCard}>Draw</button>
+                <div>{flop}</div>
              </div>
          );
     }
