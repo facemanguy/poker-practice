1cc51b951231ec5bd976d9f258583433c5aa03f0
facemanguy <john32meyer@gmail.com>
2019-09-01 18:55:21 -0700

    Updates

--- precache-manifest.f15746007d9e227a6683b3fef148235a.js
+++ precache-manifest.f15746007d9e227a6683b3fef148235a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46bbc734c9ae6b0abf7ccc1ca0492b0b",
+    "revision": "246b9730d194796e1471dd0f9f59f95e",
     "url": "/poker-practice/index.html"
   },
   {
-    "revision": "eb79085a26a08fd9d87f",
+    "revision": "11637d8977a9b0100fdd",
     "url": "/poker-practice/static/css/main.5eac0719.chunk.css"
   },
   {
-    "revision": "e72551edd94d60eb347a",
-    "url": "/poker-practice/static/js/2.bb71ae78.chunk.js"
+    "revision": "008861dc4fd1663a32a0",
+    "url": "/poker-practice/static/js/2.51ae4c65.chunk.js"
   },
   {
-    "revision": "eb79085a26a08fd9d87f",
-    "url": "/poker-practice/static/js/main.ff341495.chunk.js"
+    "revision": "11637d8977a9b0100fdd",
+    "url": "/poker-practice/static/js/main.c6ceb5ae.chunk.js"
   },
   {
     "revision": "0fdaebee0f067ce62e5b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poker-practice/precache-manifest.83550ec5a1cb6a193bf8c35a83831eee.js"
+  "/poker-practice/precache-manifest.f15746007d9e227a6683b3fef148235a.js"
 );
 
 self.addEventListener('message', (event) => {
